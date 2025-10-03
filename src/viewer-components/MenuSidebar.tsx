23e55e9ace07b053a736b145ef5de0cae1d2431c
YgorFasa98 <ygor.fasanella@gmail.com>
2025-10-03 20:30:06 +1000

    cambiato il window.location.href all'utilizzo direto con react

--- src/viewer-components/MenuSidebar.tsx
+++ src/viewer-components/MenuSidebar.tsx
@@ -1,57 +1,58 @@
-import * as React from 'react'
-import * as BUI from '@thatopen/ui'
+import * as React from 'react';
+import * as BUI from '@thatopen/ui';
+import { useNavigate } from 'react-router-dom';
 
-export function MenuSidebar () {
-    
-    BUI.Manager.init()
-    
-    const setMenuSidebar = () => {
-        const toolbar = BUI.Component.create<BUI.Toolbar>(() => {
-            return BUI.html`
-            <bim-toolbar style="justify-self: center; background-color:transparent; border:none" vertical>
-                <bim-toolbar-section label="Sidebar">
-                    <bim-button
-                        id='Home'
-                        icon="ic:round-home"
-                        tooltip-title="Home"
-                        style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
-                        @click=${() => {window.location.href = '/home'}}>
-                    </bim-button>
-                    <bim-button
-                        id='Viewer'
-                        icon="ph:cube-focus-bold"
-                        tooltip-title="BIM Viewer"
-                        style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
-                        @click=${() => {window.location.href = '/'}}>
-                    </bim-button>
-                    <bim-button
-                        id='Survey'
-                        icon="wpf:survey"
-                        tooltip-title="Survey"
-                        style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
-                        @click=${() => {window.location.href = '/survey'}}>
-                    </bim-button>
-                    <bim-button
-                        id='Info'
-                        icon="akar-icons:info-fill"
-                        tooltip-title="Info"
-                        style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
-                        @click=${() => {window.location.href = '/info'}}>
-                    </bim-button>
-                </bim-toolbar-section>
-            </bim-toolbar>
-            `;
-        })
-        
-        const menuSidebarDiv = document.getElementById('menu-sidebar-div') as HTMLElement
-        menuSidebarDiv?.appendChild(toolbar)
-    }
+export function MenuSidebar() {
+    const navigate = useNavigate();
 
-    // #region FINAL PART
     React.useEffect(() => {
-        setMenuSidebar() //set the viewer, devMode default = false
-        return () => {}
-    }, [])
+        BUI.Manager.init();
+
+        const toolbar = BUI.Component.create<BUI.Toolbar>(() => {
+        return BUI.html`
+            <bim-toolbar style="justify-self: center; background-color:transparent; border:none" vertical>
+            <bim-toolbar-section label="Sidebar">
+                <bim-button
+                id='Home'
+                icon="ic:round-home"
+                tooltip-title="Home"
+                style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
+                @click=${() => navigate('/home')}>
+                </bim-button>
+                <bim-button
+                id='Viewer'
+                icon="ph:cube-focus-bold"
+                tooltip-title="BIM Viewer"
+                style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
+                @click=${() => navigate('/')}>
+                </bim-button>
+                <bim-button
+                id='Survey'
+                icon="wpf:survey"
+                tooltip-title="Survey"
+                style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
+                @click=${() => navigate('/survey')}>
+                </bim-button>
+                <bim-button
+                id='Info'
+                icon="akar-icons:info-fill"
+                tooltip-title="Info"
+                style="display:flex; min-width:2.5rem; min-height:2.5rem; align-items:center; justify-content:center"
+                @click=${() => navigate('/info')}>
+                </bim-button>
+            </bim-toolbar-section>
+            </bim-toolbar>
+        `;
+        });
+
+        const menuSidebarDiv = document.getElementById('menu-sidebar-div') as HTMLElement;
+        menuSidebarDiv?.appendChild(toolbar);
+
+        return () => {
+        // Pulizia per evitare duplicati
+            menuSidebarDiv.innerHTML = "";
+        };
+    }, [navigate]);
 
     return (
         <div
@@ -62,7 +63,6 @@ export function MenuSidebar () {
                 margin: '0px',
                 backgroundColor: 'var(--background)',
             }}
-            >
-        </div>
+        />
     );
-};
+}
