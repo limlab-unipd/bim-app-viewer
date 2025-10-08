08f18d1345ce416a01c3950454ac484d1fba143a
YgorFasa98 <ygor.fasanella@gmail.com>
2025-10-08 17:42:30 +1100

    home page first version done

--- src/viewer-components/HomePage.tsx
+++ src/viewer-components/HomePage.tsx
@@ -1,23 +1,36 @@
 export function HomePage() {
     return (
-        <div
+        <div 
             style={{
-                flex: 1,
+                height: '100vh',
                 padding: "2rem",
-                overflowY: "auto",
+                boxSizing: 'border-box',
                 fontFamily: "Arial, sans-serif",
-                backgroundColor: 'var(--background)',
-                color: "#333",
-            }}
-            >
+                color: "#fff",
+                display: 'flex',
+                flexDirection: 'column',
+                overflow: 'hidden',
+                
+                backgroundImage: "linear-gradient(rgba(15, 15, 15, 0.4), rgba(15, 15, 15, 0.4)), url('/PNG/home-image.png')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+            }}>
 
-            <header style={{ textAlign: "center", marginBottom: "2rem" }}>
-                <h1 className="pages-header">HOME PAGE</h1>
-                <p style={{ fontSize: "1.1rem", color: "white" }}>
-                    Coming soon ...
-                </p>
+            <header style={{ display:'flex', flexDirection:'column', textAlign:"left", flex: 1, marginBottom:"2rem", marginLeft:"2rem"}}>
+                <h1 className="pages-header home-title" style={{ fontWeight: "bold", margin: 0 }}>VISUALIZE</h1>
+                <h1 className="pages-header home-title" style={{ margin: 0 }}>YOUR</h1>
+                <h1 className="pages-header home-title" style={{ margin: 0 }}>AEC</h1>
+                <h1 className="pages-header home-title" style={{ margin: 0 }}>DATA</h1>
+
+                <div style={{ display:'flex', flex:1, alignItems:'center', justifyContent:'left', textAlign:'left' }}>
+                    <div style={{ fontSize: "clamp(1.5rem, 3vw, 3rem)",  fontFamily: "Roboto, sans-serif", color: "white", margin: 0, maxWidth:"70%"}}>
+                        <p>Explore <strong>OpenBIM</strong> standards and web technologies</p>
+                        <p>transforming interconnected AEC <strong>data</strong> into</p>
+                        <p>interactive and meaningful 3D <strong>visualizations</strong></p>
+                    </div>
+                </div>
             </header>
-
         </div>
     );
 }
