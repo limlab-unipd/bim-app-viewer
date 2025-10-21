215d9b679652d37711ce156f20126d1dadc4b8ab
YgorFasa98 <ygor.fasanella@gmail.com>
2025-10-21 15:54:54 +1100

    added information in ingo page. added R to openBIM in home page.

--- src/viewer-components/HomePage.tsx
+++ src/viewer-components/HomePage.tsx
@@ -24,8 +24,8 @@ export function HomePage() {
                 <h1 className="pages-header home-title" style={{ margin: 0 }}>DATA</h1>
 
                 <div style={{ display:'flex', flex:1, alignItems:'center', justifyContent:'left', textAlign:'left', marginTop:'32px'}}>
-                    <div style={{ fontSize: "clamp(1.5rem, 2.5vw, 2.5rem)", color: "rgba(224, 224, 224, 0.75)", margin: 0, maxWidth:"100%"}}>
-                        <p className="home-description">Explore <strong className="home-description">OpenBIM</strong> standards and web technologies</p>
+                    <div style={{ fontSize: "clamp(1rem, 2.5vw, 2.5rem)", color: "rgba(224, 224, 224, 0.75)", margin: 0, maxWidth:"100%"}}>
+                        <p className="home-description">Explore <strong className="home-description">openBIM®</strong> standards and web technologies</p>
                         <p className="home-description">transforming interconnected AEC <strong className="home-description">data</strong> into</p>
                         <p className="home-description">interactive and meaningful 3D <strong className="home-description">visualizations</strong></p>
                     </div>
