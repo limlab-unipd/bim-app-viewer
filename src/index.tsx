4f9a934781947cf619a8827da5f7ca8439abca62
Ygor Fasanella <ygor.fasanella@gmail.com>
2026-09-23 15:30:32 +0200

    RELEASE: - version 0.1.0 for phd thesis submission - first ever deplay of urban viewer. this project in vercel is only for phd thesis release, in future it will be updated and merged into the main project bim-app-viewer.vercel.app

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as ReactDOM from 'react-dom/client'
 import * as Router from 'react-router-dom'
+import * as React from 'react'
 
 import { InfoPage } from './viewer-components/InfoPage'
 import { MenuSidebar } from './viewer-components/MenuSidebar'
@@ -12,17 +13,25 @@ import { UrbanViewer } from './viewer-components/UrbanViewer'
 //#region REACT COMPONENTS
 const rootElement = document.getElementById('app') as HTMLDivElement
 const appRoot = ReactDOM.createRoot(rootElement)
+
+function CostViewerRedirect(page: string) {
+    React.useEffect(() => {
+        window.location.replace(page)
+    }, [])
+    return null
+}
+
 BUI.Manager.init()
 appRoot.render(
     <>
     <Router.BrowserRouter>
         <MenuSidebar></MenuSidebar>
         <Router.Routes>
-            <Router.Route path='/home' element={ <HomePage /> } />
-            <Router.Route path='/' element={ <MainViewer /> } />
+            <Router.Route path='/home' element={ CostViewerRedirect('https://bim-app-viewer.vercel.app/home') } />
+            <Router.Route path='/' element={ CostViewerRedirect('https://bim-app-viewer.vercel.app') } />
             <Router.Route path='/urban-viewer' element={ <UrbanViewer /> } />
-            <Router.Route path='/survey' element={ <SurveyPage /> } />
-            <Router.Route path='/info' element={ <InfoPage /> } />
+            <Router.Route path='/survey' element={ CostViewerRedirect('https://bim-app-viewer.vercel.app/survey') } />
+            <Router.Route path='/info' element={ CostViewerRedirect('https://bim-app-viewer.vercel.app/info') } />
         </Router.Routes>
     </Router.BrowserRouter>
     </>
